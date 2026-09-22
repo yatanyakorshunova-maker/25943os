65dd8cdcc692ef8bbe0c87d1432d62231421a208
Tatiana Korshunova <t.korshunova@g.nsu.ru>
2026-09-22 19:16:40 +0700

    Переписал код проще

--- t.korshunova/lab_time/extime.c
+++ t.korshunova/lab_time/extime.c
@@ -1,28 +1,28 @@
-#include <sys/types.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
-extern char *tzname[];
-
 int main()
 {
     time_t now;
-    struct tm *sp;
+    struct tm my_time;
 
     setenv("TZ", "PST8PDT", 1);
     tzset();
 
-    (void) time(&now);
+    time(&now);
 
     printf("%s", ctime(&now));
 
-    sp = localtime(&now);
+    my_time = *localtime(&now);
+
     printf("%d/%d/%d %d:%02d %s\n",
-        sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year + 1900, sp->tm_hour,
-        sp->tm_min, tzname[sp->tm_isdst]);
+        my_time.tm_mon + 1,
+        my_time.tm_mday,
+        my_time.tm_year + 1900,
+        my_time.tm_hour,
+        my_time.tm_min,
+        my_time.tm_isdst ? "PDT" : "PST");
 
-    exit(0);
+    return 0;
 }
-
